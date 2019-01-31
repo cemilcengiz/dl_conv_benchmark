73bc18395a1ee521f6307e473802e4130293d2df
ccengiz17 <ccengiz17@login03.kuacc.ku.edu.tr>
2019-01-31 16:17:24 +0300

    add mxnet cnn

---
+++ mxnetnet.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+import time
+import mxnet as mx
+from mxnet import nd, autograd, gluon
+
+class MXNetNet():
+    # Filters come as a 4D Tensor of  [out_channels, in_channels, fil_height, fil_width]
+    def __init__(self, convfilters, poolings=False):
+        self.conv_filters_ = []
+        for w_array in convfilters:
+            # MXNet nd.Convolution takes filter as a 4D Tensor of  [out_channels, in_channels, fil_height, fil_width]
+            w_tensor = mx.nd.array(np.copy(w_array))
+            self.conv_filters_.append(w_tensor)
+            
+        self.poolings_ = poolings
+        
+        
+    def forward(self, x):
+        # MXNet nd.Convolution takes in a 4D Tensor of [nSamples x nChannels x Height x Width]
+        x = mx.nd.array(x)
+        
+        layer = 0
+        t0 = time.time()
+        for w in self.conv_filters_:           
+            print("layer:", layer)
+            print("size of x before:", x.shape)
+            print("size of filter :", w.shape)
+            
+            #x = nd.Convolution(data=x, weight=w)
+            x = nd.Convolution(data=x, weight=w, kernel=w.shape[-2:], num_filter=w.shape[0], no_bias=True)
+            print("size of x after filter:", x.shape)
+
+            if self.poolings_:
+                window = (self.poolings_[layer], self.poolings_[layer])
+                x = nd.Pooling(data=x, pool_type="max", kernel=window, stride=window)
+                print("size of x after pool:", x.shape)
+                print(" ")
+                layer += 1
+        
+        t1 = time.time()
+        #return (x.numpy(), (t1-t0))
+        return (x.asnumpy(), (t1-t0), self.conv_filters_)
